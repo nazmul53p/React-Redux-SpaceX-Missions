ebe9cad7bed71f674f5ab7b6d0bb3597ca4a1c53
nazmul53p <nazmul2018s@gmail.com>
2021-10-07 22:58:52 +0600

    initial setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import App from './App';
+import './assets/css/index.css';
+import Home from './pages/Home';
 import reportWebVitals from './reportWebVitals';
+// import Store from './store/Store';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={Store}>
+            <App>
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path="/home" component={Home} />
+                        <Redirect from="/" to="/home" />
+                    </Switch>
+                </BrowserRouter>
+            </App>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
